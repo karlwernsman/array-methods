dd0a0d3da0ccd3a7e495c44c2e70decd04e7350b
karl wernsman <wernsmank@gmail.com>
2023-01-18 09:51:56 -0800

    map 2 test passing

--- map.js
+++ map.js
@@ -2,13 +2,19 @@
 // OUTPUT: an array of the names
 // REQS: use .map
 
-export const getCountryNames = (countries) => {};
+export const getCountryNames = (countries) => {
+  const countryNames = countries.map((country) => country.name);
+  return countryNames;
+};
 
 // INPUT: the array of countries from data.js
 // OUTPUT: an array of the names and their iso2 country codes using .map
 // REQS: use .map
 
-export const getCountryCodes = (countries) => {};
+export const getCountryCodes = (countries) => {
+  const countryNameAndCode = countries.map((country) => [country.name, country.iso2]);
+  return countryNameAndCode;
+};
 
 // INPUT: the array of shoes from data.js
 // OUTPUT: an array of the brands
