a6ebd863155334d6e965239dafe7c5448f3c4a28
karl wernsman <wernsmank@gmail.com>
2023-01-18 12:29:37 -0800

    1st reduce test passing

--- filter.js
+++ filter.js
@@ -1,15 +1,15 @@
 // INPUT: the array of countries from data.js
 // OUTPUT: only countries in Oceania
 // REQS: use .filter
-
 export const filterOceania = (countries) => {
   return countries.filter((country) => country.continent === 'Oceania');
 };
+//can also do
+//return countries.filter((country) => country.continent?.includes('Oceania'));
 
 // INPUT: the array of countries from data.js
 // OUTPUT: only countries whose name that include the letters 'au'
 // REQS: use .filter
-
 export const filterAu = (countries) => {
   return countries.filter((country) => country.name.includes('au'));
 };
@@ -28,3 +28,7 @@ export const getYoungDogNames = (dogs) => {
   const youngDogs = dogs.filter((dog) => dog.age <= 2);
   return youngDogs.map((dog) => dog.name);
 };
+//can also do
+//return filterYoungDogs(dogs).map((dog) => dog.name);
+//or
+//return dogs.filter((dog) => dog.age <= 2).map((dog) => dog.name);
